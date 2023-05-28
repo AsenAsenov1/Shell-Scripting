e3b1874cc07f4da81921998867fc5db49737b431
Asen Asenov <asen991@gmail.com>
2023-05-28 21:51:07 +0300

    UPLOAD: module 3

---
+++ shell_scripting_course/module_3/exercise1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# DEFINE THE FUNCTION
+function file_count() {
+    local NUMBER_OF_FILES=$(ls | wc -l)
+    echo "The number of files in the present working directory is: ${NUMBER_OF_FILES}"
+}
+
+# CALL THE FUNCTION
+file_count
---
+++ shell_scripting_course/module_3/exercise2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# DEFINE THE FUNCTION
+function file_count() {
+    local DIR_PATH=$1
+    local NUMBER_OF_FILES=$(ls $DIR_PATH | wc -l)
+    echo "${DIR_PATH}:"
+    echo "    ${NUMBER_OF_FILES}"
+}
+
+# CALL THE FUNCTION
+file_count /etc
+file_count /var
+file_count /usr/bin
